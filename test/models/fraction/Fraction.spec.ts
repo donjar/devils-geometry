6c5e44fb54765eb73c53dfa7e4de7de8293ddf81
Herbert Ilhan Tanujaya <mail@herbert.id>
2017-07-16 17:52:59 +0800

    get coverage and travis cache working

--- test/models/fraction/Fraction.spec.ts
+++ test/models/fraction/Fraction.spec.ts
@@ -1,7 +1,7 @@
 import Fraction from '../../../app/models/fraction/Fraction'
 import Integer from '../../../app/models/fraction/Integer'
 
-describe('Fraction', () => {
+describe('constructor', () => {
   it('is constructed by passing a numerator or denumerator, both integer or number', () => {
     const frac1 = new Fraction(3, 5)
     const frac2 = new Fraction(new Integer(3), 5)
@@ -32,5 +32,34 @@ describe('Fraction', () => {
 
   it('throws an error on zero denumerator', () => {
     expect(() => { new Fraction(3, 0) }).toThrow(Error)
+  })
+})
+
+describe('math operators', () => {
+  const frac1 = new Fraction(3, 7)
+  const frac2 = new Fraction(2, 5)
+
+  it('adds by calling add', () => {
+    const sum = frac1.add(frac2)
+    expect(sum.numerator.number).toEqual(29)
+    expect(sum.denominator.number).toEqual(35)
+  })
+
+  it('subtracts by calling sub', () => {
+    const sum = frac1.sub(frac2)
+    expect(sum.numerator.number).toEqual(1)
+    expect(sum.denominator.number).toEqual(35)
+  })
+
+  it('multiplies by calling mul', () => {
+    const prod = frac1.mul(frac2)
+    expect(prod.numerator.number).toEqual(6)
+    expect(prod.denominator.number).toEqual(35)
+  })
+
+  it('divides by calling div', () => {
+    const quotient = frac1.div(frac2)
+    expect(quotient.numerator.number).toEqual(15)
+    expect(quotient.denominator.number).toEqual(14)
   })
 })
